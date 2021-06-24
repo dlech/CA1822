f25c997aaf5a6ef847f7fc5752e49d8ebf82c077
David Lechner <david@lechnology.com>
2021-06-23 20:37:02 -0500

    first commit

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CA1822
+{
+    public partial class Class1
+    {
+        public partial Task Example(CancellationToken token = default);
+        // -----------------^
+        // incorrectly triggers CA1822
+    }
+
+    partial class Class1
+    {
+        private readonly int timeout;
+
+        public Class1(int timeout)
+        {
+            this.timeout = timeout;
+        }
+        
+        public async partial Task Example(CancellationToken token)
+        {
+            await Task.Delay(timeout, token);
+        }
+    }
+}
